b15a673db30352e79db47c5ab892d4c546631a9c
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-09 21:45:58 +0000

    Sat, Aug 9, 2025, 10:45 PM +01:00

--- 78957963/week_2/search_str.c
+++ 78957963/week_2/search_str.c
@@ -5,7 +5,7 @@
 int main(void)
 {
     string strings[] = {"battleship", "boot", "cannnon", "iron", "thimple", "top hat"};
-    printf("%zu\n", strlen(strings[0]));
+    printf("%s\n", strings[]);
     // string input_string = get_string("String: ");
     // for(int i = 0; i < len(input_string); i++);
     return 0;
