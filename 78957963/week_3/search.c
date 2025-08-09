8aac6050e581c8de1670b526346fe0ed7013bd57
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-09 18:49:47 +0000

    Sat, Aug 9, 2025, 7:49 PM +01:00

--- 78957963/week_3/search.c
+++ 78957963/week_3/search.c
@@ -6,12 +6,15 @@ int main(void)
 {
     // create a array
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
-    int n = get_int("Enter a number: \n");
+
     bool found = false;
     int max_attempts = 3;
 
     for (int count = 1; count <= max_attempts; count++)
     {
+
+        int n = get_int("Attempt %i - Enter a number: \n", count);
+
         for (int i = 0; i < 7; i++)
         {
             if (numbers[i] == n)
