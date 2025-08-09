bd8a5c67018fff5dd3eac0318bfaa4eb490f6edf
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-09 21:57:28 +0000

    Sat, Aug 9, 2025, 10:57 PM +01:00

--- 78957963/week_2/search_str.c
+++ 78957963/week_2/search_str.c
@@ -6,10 +6,10 @@ int main(void)
 {
     string strings[] = {"battleship", "boot", "cannnon", "iron", "thimple", "top hat"};
     printf("%s\n", strings[]);
-    string input_string = get_string("String: ");
+    
 
     int word_count = sizeof(strings) / sizeof(strings[0]);
-    for(int i = 0; i < len(input_string); i++)
+    for(int i = 0; i < len(word_count); i++)
     {
         printf("%s\n", strings[i]);
     }
