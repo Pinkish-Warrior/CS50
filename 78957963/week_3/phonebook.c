306cffc5b2d6757d14a2750114d6ec3198fbf27f
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-12 11:17:44 +0000

    Tue, Aug 12, 2025, 12:17 PM +01:00

--- 78957963/week_3/phonebook.c
+++ 78957963/week_3/phonebook.c
@@ -2,24 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
+// int main(void)
+// {
+//     string names[] = {"Yuliia", "David", "John"};
+//     string numbers[] = {"+447525844889", "+447708817550", "+447999976543"};
+
+//     string name = get_string("Name: ");
+//     for (int i = 0; i < sizeof(names) / sizeof(names[0]); i++)
+//     {
+//         if (strcmp(names[i], name) == 0)
+//         {
+//             printf("Found %s\n", numbers[i]);
+//             // number found
+//             return 0;
+//         }
+//     }
+//     printf("Not Found!\n");
+//     return 1;
+}
+
+// Refactoring
+
+// structure
+
+typedef struct{
+    name:
+    number:
+}
+
 int main(void)
 {
-    string names[] = {"Yuliia", "David", "John"};
-    string numbers[] = {"+447525844889", "+447708817550", "+447999976543"};
 
-    // refactoring :
-
-
-    string name = get_string("Name: ");
-    for (int i = 0; i < sizeof(names) / sizeof(names[0]); i++)
-    {
-        if (strcmp(names[i], name) == 0)
-        {
-            printf("Found %s\n", numbers[i]);
-            // number found
-            return 0;
-        }
-    }
-    printf("Not Found!\n");
-    return 1;
 }
