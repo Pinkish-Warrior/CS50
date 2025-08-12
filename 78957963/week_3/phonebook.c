2662366533b65e910c66d826cd72e0b2794248bb
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-12 09:00:44 +0000

    Tue, Aug 12, 2025, 10:00 AM +01:00

--- 78957963/week_3/phonebook.c
+++ 78957963/week_3/phonebook.c
@@ -2,7 +2,16 @@
 #include<cs50.h>
 
 int main(void)
-{
     string name[] = {"Yuliia", "Davis", "John"};
     string numbers[] = { "+447525844889","+447708817550","+447999976543"};
-}
+
+    string name 
+    for (i = 0; i < names[1]; i++)
+    {
+        if(strcmp names[i], name) == 0)
+        {
+            printf("Found %s/n", numbers[i]);
+            // number found
+            return 0;
+        }
+    }
