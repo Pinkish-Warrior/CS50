5c7818a1ff099bdb77f85e275a317d5ee44374d5
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-12 10:33:41 +0000

    Tue, Aug 12, 2025, 11:33 AM +01:00

--- 78957963/week_3/phonebook.c
+++ 78957963/week_3/phonebook.c
@@ -8,7 +8,7 @@ int main(void)
     string numbers[] = {"+447525844889", "+447708817550", "+447999976543"};
 
     string name = get_string("Name: ");
-    for (int i = 0; i < names[i]-1; i++)
+    for (int i = 0; i < sizeof(names[]); i++)
     {
         if (strcmp(names[i], name) == 0)
         {
