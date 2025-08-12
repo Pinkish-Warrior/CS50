41b8bad8dd9f344ac2511c8946f75a73f79c52da
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-12 10:24:35 +0000

    Tue, Aug 12, 2025, 11:24 AM +01:00

--- 78957963/week_3/phonebook.c
+++ 78957963/week_3/phonebook.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-#include<cs50.h>
-#include<string.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string names[] = {"Yuliia", "Davis", "John"};
-    string numbers[] = { "+447525844889","+447708817550","+447999976543"};
+    string numbers[] = {"+447525844889", "+447708817550", "+447999976543"};
 
     string name = get_string("Name: ");
     for (int i = 0; i < strlen(names[i]); i++)
     {
-        if(strcmp (names[i], name) == 0)
+        if (strcmp(names[i], name) == 0)
         {
             printf("Found %s/n", numbers[i]);
             // number found
