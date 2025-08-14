e35fe5fc302e2e25c08cc7d07d182a54d9cd26da
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-14 12:52:10 +0000

    Thu, Aug 14, 2025, 1:52 PM +01:00

--- 78957963/week_4/addresses.c
+++ 78957963/week_4/addresses.c
@@ -2,4 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
+// & address operators lecture
+// * dereference operator
+// %p pointers
+
+// int *p = &n - it reads left to right => whatever value of n is get it and asigned to the veriable p, which is a pointer this case an integer)
+
+int main(void)
+{
+    int n = 50;
+    int *p = &n; //conventional way
+
+    printf("%i %d\n", *p, n);
+}
 
