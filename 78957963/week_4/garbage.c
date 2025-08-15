615d99cc19a824b904daf4f356c3bab013fd6130
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 10:18:18 +0000

    Fri, Aug 15, 2025, 11:18 AM +01:00

---
+++ 78957963/week_4/garbage.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int scores[1024];
+
+    for (int i = 0; i < 1024; i++)
+    {
+        printf("%i\n", scores[i]);
+    }
+
+
+
+    printf("Hello World from C");
+    // Allways remember to free up memory before exiting the program
+}
