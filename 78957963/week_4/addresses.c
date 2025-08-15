6bccc0c00896a4e0befbf5e8aa15c4654b84bc60
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 09:08:18 +0000

    Fri, Aug 15, 2025, 10:08 AM +01:00

--- 78957963/week_4/addresses.c
+++ 78957963/week_4/addresses.c
@@ -13,6 +13,16 @@ int main(void)
     int n = 50;
     int *p = &n; //conventional way
 
-    printf("%i %d\n", *p, n);
+    // %i => integer pointer of p = 50
+    // %d => dereferencing n = 50
+    // %p => pointer n = 50
+
+    printf("%i %p\n", *p, &n);
+    // 50 0x7ffe15daef0c
+    printf("%p %i\n", p, n);
+    // 0x7ffe15daef0c 50
 }
 
+// understanding pointers and format specifiers in C.
+// %i and %d are used for integers. When we use *p with %i or %d, you’re printing the value that p points to.
+// %p is used for pointers (addresses). When we use &n with %p, you’re printing the address of n. When you use p with %p, you’re printing the address stored in p, which is also the address of n.
