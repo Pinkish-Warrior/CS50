38214b92f2ea39c89e68e87699880909e95aea64
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 09:36:33 +0000

    Fri, Aug 15, 2025, 10:36 AM +01:00

--- 78957963/week_4/memory.c
+++ 78957963/week_4/memory.c
@@ -6,21 +6,26 @@ int main(void)
     // memory allocation for the pointer of size per following
     int *x = malloc(3 * sizeof(int));
 
+    if (x == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
     // values being stored in memory location pointed by x.
     x[0] = 72;
     x[1] = 73;
     x[2] = 33;
 
     if (x != NULL)
-    {
-        printf("Memory aloccated susscefully");
-    }
+
+    printf("Memory aloccated susscefully");
+
     // freeing up memory used(preventing memory leaks)
     free(x);
 
     // avoiding dangling pointers
-    else
-    {
-        x = NULL
-        return 0;
-    }
+    x = NULL;
+    return 0;
+
+}
