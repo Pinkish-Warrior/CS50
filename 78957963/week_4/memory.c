0fd8083235e2924c981e3510d5fadce0739ba494
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 09:55:38 +0000

    Fri, Aug 15, 2025, 10:55 AM +01:00

--- 78957963/week_4/memory.c
+++ 78957963/week_4/memory.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// demostrating the use of valgrind ./memory
+// demostrating the usage of > valgrind ./memory
 // use valdrind and the compiled file to check for verboses related to memory in c
 
 int main(void)
