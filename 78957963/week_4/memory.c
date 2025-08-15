a3ac38c354ca76c43cfc95f0a0d6fe438a1b6776
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 09:10:24 +0000

    Fri, Aug 15, 2025, 10:10 AM +01:00

--- 78957963/week_4/memory.c
+++ 78957963/week_4/memory.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
+    // memory allocation for the pointer
     int *x = malloc(3 * sizeof(int));
     x[0] = 72;
     x[1] = 73;
     x[2] = 33;
+    // free up the memore used
     free(x);
 }
