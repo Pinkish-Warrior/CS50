0e2dd12dbb7c6de9e7fc980ad47672a9ff3101f4
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 09:17:20 +0000

    Fri, Aug 15, 2025, 10:17 AM +01:00

--- 78957963/week_4/memory.c
+++ 78957963/week_4/memory.c
@@ -3,12 +3,14 @@
 
 int main(void)
 {
-    // memory allocation for the pointer
-    
+    // memory allocation for the pointer of size per following
     int *x = malloc(3 * sizeof(int));
+
+    // integers to be alocated in memory
     x[0] = 72;
     x[1] = 73;
     x[2] = 33;
+
     // free up the memore used
     free(x);
 }
