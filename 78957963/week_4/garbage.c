4b8b6d8e3870dc1a10d9fa42cba88a392e2cc08a
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-15 10:29:46 +0000

    Fri, Aug 15, 2025, 11:29 AM +01:00

--- 78957963/week_4/garbage.c
+++ 78957963/week_4/garbage.c
@@ -10,8 +10,11 @@ int main(void)
     // pointer memory allocation to a pointee
     x = malloc(sizeof(int));
 
+    // assigned integer to a pointer x
     *x = 42;
 
+    // now assigning the value of x to y
     y = x;
 
+    // following assigning this value to a pointer y
     *y = 13;
