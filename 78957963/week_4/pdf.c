8c41bfeb842c75165f0d66514d507cd3c4d15744
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-26 21:21:29 +0000

    Tue, Aug 26, 2025, 11:21 PM +02:00

--- 78957963/week_4/pdf.c
+++ 78957963/week_4/pdf.c
@@ -2,48 +2,48 @@
 #include <stdio.h>
 #include <string.h>
 
-// Solution A
+// Solution A(my solution)
 
 // read the name of a file and return yes for those .pdf
 // check if the sequence is 0x25 0x50 0x44 0x46
-int hasExtension(const char *filename, const char *extension)
-{
-    // find the last dot in the filename
-    const char *dot = strrchr(filename, '.');
+// int hasExtension(const char *filename, const char *extension)
+// {
+//     // find the last dot in the filename
+//     const char *dot = strrchr(filename, '.');
 
-    // no dot means no extension
-    if (!dot || dot == filename)
-    {
-        printf("%s\n", dot);
-        return 0;
-    }
-    // comparing to the extension
-    return strcmp(dot +1, extension) == 0;
-}
-int main(void)
-{
-    string filename = get_string("Provide a name to check: \n");
-    string extension = get_string("Extension: ");
-    {
-        if(hasExtension(filename, extension))
-        {
-            printf("True\n");
-        }
-        else
-        {
-            printf("Try again!\n");
-        }
-    }
-}
+//     // no dot means no extension
+//     if (!dot || dot == filename)
+//     {
+//         printf("%s\n", dot);
+//         return 0;
+//     }
+//     // comparing to the extension
+//     return strcmp(dot +1, extension) == 0;
+// }
+// int main(void)
+// {
+//     string filename = get_string("Provide a name to check: \n");
+//     string extension = get_string("Extension: ");
+//     {
+//         if(hasExtension(filename, extension))
+//         {
+//             printf("True\n");
+//         }
+//         else
+//         {
+//             printf("Try again!\n");
+//         }
+//     }
+// }
 
-/* Guidance
+/* Guidance */
 // Solution B
 // int main(int argc, string argv[])
 int main(int argc, string argv[])
 {
 
     // Open file
-    FILE *input = fopen(argv[1], "r")
+    FILE *input = fopen(argv[1], "r");
 
     // Create buffer for file
     uint8_t buffer[4]; //0x25 0x50 0x44 0x46
@@ -52,7 +52,7 @@ int main(int argc, string argv[])
     uint8_t signature[] = {0x25, 0x50, 0x40, 0x46};
 
     // Read first four bytes from the file
-    fread(buffer, sizeof(uint8_t), 4, input)
+    fread(buffer, sizeof(uint8_t), 4, input);
 
     // Check or the first four bytes again signature bytes
     for (int i = 0; i < 4; i++)
@@ -64,9 +64,9 @@ int main(int argc, string argv[])
         }
     }
     // Sucess!
-    printf("This is a PDF!\n")
+    printf("This is a PDF!\n");
 
     // Close file
-    fclose(input)
+    fclose(input);
 }
-*/
+
