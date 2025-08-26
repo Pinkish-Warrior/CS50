51bcfc1dccfb6af17e754b89aa7a91824dfa9df8
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-26 20:16:47 +0000

    Tue, Aug 26, 2025, 10:16 PM +02:00

--- 78957963/week_4/pdf.c
+++ 78957963/week_4/pdf.c
@@ -33,14 +33,21 @@ FILE *input = fopen(argv[1], "r")
 // Create buffer for file
 uint8_t buffer[4]; //0x25 0x50 0x44 0x46
 
-// Create an array of the given signature bytes
-uint8_t signature[] = {0x25, 0x50, 0x40, 0x46}
+// Create an array of the given signature bytes (%pdf = 0x25, 0x50, 0x40, 0x46)
+uint8_t signature[] = {0x25, 0x50, 0x40, 0x46};
 
 // Read first four bytes from the file
 fread(buffer, sizeof(uint8_t), 4, input)
 
 // Check or the first four bytes again signature bytes
-
+for (int i = 0; i < 4; i++)
+{
+    if (signature[i] != buffer[i])
+    {
+        printf("This is not a pdf file!\n");
+        return 0;
+    }
+}
 // Sucess!
 
 // Close file
