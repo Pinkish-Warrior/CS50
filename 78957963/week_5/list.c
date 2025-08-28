08e8a833b01df1e6bb40afeb4f5a26e0b9c6ee3c
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-28 11:02:10 +0000

    Thu, Aug 28, 2025, 12:02 PM +01:00

--- 78957963/week_5/list.c
+++ 78957963/week_5/list.c
@@ -3,14 +3,18 @@
 
 int main(void)
 {
-    int list[3];
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+    /* Initial approach */
+    // int list[3];
+    // list[0] = 1;
+    // list[1] = 2;
+    // list[2] = 3;
 
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%i\n", list[i]);
-    }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("%i\n", list[i]);
+    // }
+
+    /* secundary approach */
+    
 
 }
