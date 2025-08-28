aa141da31afa619e1963ded5e886744b06054b9c
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-28 14:30:53 +0000

    Thu, Aug 28, 2025, 3:30 PM +01:00

--- 78957963/week_5/list.c
+++ 78957963/week_5/list.c
@@ -30,6 +30,12 @@ int main(void)
     node *list = NULL;
     for (int i = 0; i < 3; i++)
     {
-
+        node *n = malloc(sizeof(node));
+        if(n == NULL)
+        {
+            return 1;
+        }
+        (*n).number = get_int("Number: ");
+        (*n).next = NULL;
     }
 }
