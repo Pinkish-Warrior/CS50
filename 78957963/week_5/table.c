b727f1ea134a1f0ec29dad2fa2bd2857d475ad98
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-29 17:41:57 +0000

    Fri, Aug 29, 2025, 6:41 PM +01:00

--- 78957963/week_5/table.c
+++ 78957963/week_5/table.c
@@ -30,5 +30,6 @@ int main(void)
 int hash(string phrase)
 {
     // return a number 0-25 depending on the the first char of the phrase
-    return toupper(phrase [0]) - 'A';
+    // return toupper(phrase [0]) - 'A';
+    return tolower(phrase[0] - 'a');
 }
