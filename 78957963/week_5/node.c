5d0994b2d8eb719b66e2c5e2e3b8f2d1a81371a6
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-29 10:19:12 +0000

    Fri, Aug 29, 2025, 11:19 AM +01:00

--- 78957963/week_5/node.c
+++ 78957963/week_5/node.c
@@ -11,7 +11,7 @@ typedef struct node
 {
     int number;          // this is the "cargo" box
     struct node *next;   // this is the "connector" to the next train block
-} node;
+} node; // nickname 
 
 /* Step by step:
 
