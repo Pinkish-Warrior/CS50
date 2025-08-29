aac0dcecf4acf93de644b3ab15e1abd0cda41afd
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-29 10:12:59 +0000

    Fri, Aug 29, 2025, 11:12 AM +01:00

--- 78957963/week_5/node.c
+++ 78957963/week_5/node.c
@@ -14,11 +14,19 @@ typedef struct node
 } node;
 
 /* Step by step:
-struct node { ... };
+
+✅ struct node { ... };
 This is like a blueprint for making a train block. Every block will have two parts inside it.
-int number;
+
+✅ int number;
 This is the cargo inside the block. It stores a number (like 5, 42, or 100 or chars depending on the type).
-struct node *next;
-This is the connector piece that lets this block hook onto the next block. It doesn’t hold a number itself, but instead points to where the next block is sitting.
-typedef ... node;
+Or even more flexibly, we could make it carry any type we want:
+float value; → for decimal numbers (e.g., 3.14)
+char word[50]; → for words/strings up to 49 letters
+void *data; → super flexible, can point to any kind of data
+
+✅ struct node *next;
+This is the connector piece that lets this block hook onto the next block. It doesn’t hold a "value" itself, but instead points to where the next block is sitting.
+
+✅ ♖typedef ... node;
 This is just giving the block a short nickname. Instead of saying struct node every time, you can now just say node. /*
