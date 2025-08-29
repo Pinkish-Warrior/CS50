eee506968b934587da153e18b9a297cf5997dbce
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-08-29 16:57:47 +0000

    Fri, Aug 29, 2025, 5:57 PM +01:00

--- 78957963/week_5/table.c
+++ 78957963/week_5/table.c
@@ -1,4 +1,15 @@
 #include<cs50.g>
 #include<stdio.h>
+#include<stdib.h>
 #include<string.h>
-#include<>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+} node;
+
+node *table[26];
+
+int hash(string phrase);
+bool unload(node *list);
