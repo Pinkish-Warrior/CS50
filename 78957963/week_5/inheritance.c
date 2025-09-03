eb53da019f88ab04d23a32cce5979b672736be93
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-09-03 12:46:09 +0000

    Wed, Sep 3, 2025, 1:46 PM +01:00

--- 78957963/week_5/inheritance.c
+++ 78957963/week_5/inheritance.c
@@ -5,5 +5,18 @@
 
 int main(void)
 {
-    
+
+}
+
+
+person *create_family(int generations)
+{
+    // TODO: Alocate memory for new person
+
+    // If there are still generetions left to create
+    if(generations > 1)
+    {
+        // Create two new parents for the current person by callening it recursivily
+        person *parent) = create_family(generations -1)
+    }
 }
