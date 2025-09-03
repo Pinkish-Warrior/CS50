f01f476f24c2e52bbac877b6729d45808b3e5f10
Tania Machado Santana <78957963+Pinkish-Warrior@users.noreply.github.com>
2025-09-03 13:25:31 +0000

    Wed, Sep 3, 2025, 2:25 PM +01:00

--- 78957963/week_5/inheritance.c
+++ 78957963/week_5/inheritance.c
@@ -46,6 +46,18 @@ person *create_family(int generations)
 
     // TODO: Return newly created person
     return NULL;
-    
 
+    // Free 'p' and all ancestors of 'p'.
+    void free_family(person *p)
+    {
+        // TODO: Handle base case
+
+
+        // TODO: Free perents recursively
+
+
+        // TODO: Free child
+    }
+
+    // print each family member and their alleles
 }
